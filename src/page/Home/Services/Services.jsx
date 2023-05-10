ab62e2a7b49ce4bb44e0c0224ae9513d81295b67
himaloyash <himaloyashis98@gmail.com>
2023-05-10 13:36:41 +0600

    card added

--- src/page/Home/Services/Services.jsx
+++ src/page/Home/Services/Services.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from '../ServiceCard/ServiceCard';
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(()=>{
+        fetch('service.json')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setServices(data)
+        })
+    },[])
+
+
     return (
         <div>
-            <div className=''>
+            <div className='text-center space-y-2'>
                 <h2 className='text-orange-600'>Service</h2>
-                <h2 className='text-4xl'>Our Service Area</h2>
+                <h2 className='text-4xl font-bold'>Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+
+            <div>
+                {
+                    services.map(service=> <ServiceCard key={service._id}></ServiceCard> )
+                }
             </div>
         </div>
     );
