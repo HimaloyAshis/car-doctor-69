67993e6c6c0f3371039a3c04e1242071b7c4be84
himaloyash <himaloyashis98@gmail.com>
2023-05-13 22:14:59 +0600

    almost done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -7,52 +7,74 @@ export const AuthContext = createContext(null)
 
 const auth = getAuth(app)
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
-const [user, setUser] = useState(null)
-const [loading, setLoading] = useState(true)
-const googleProvider = new GoogleAuthProvider()
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
 
-const createUser  = (email, password) =>{
-    setLoading(true)
-    return createUserWithEmailAndPassword(auth, email, password)
-}
-
-const logIn =(email, password)=>{
-    setLoading(true)
-    return signInWithEmailAndPassword(auth, email, password)
-}
-
-const logOut =()=>{
-    setLoading(true)
-    return signOut(auth)
-}
-
-const googleSignIn=()=>{
-    setLoading(true)
-    return signInWithPopup(auth, googleProvider)
-}
-
-useEffect(()=>{
-  const unsubscribe =  onAuthStateChanged(auth, currentUser=>{
-        setUser(currentUser)
-        console.log('current user', currentUser)
-        setLoading(false)
-    })
-    return ()=>{
-        return unsubscribe()
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
-},[])
 
-const userInfo = {
-    user,
-    loading,
-    createUser,
-    logIn,
-    logOut,
-    googleSignIn,
+    const logIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
 
-}
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    const googleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log('current user', currentUser)
+            setLoading(false)
+            if (currentUser && currentUser.email) {
+                const loggedUser = {
+                    email:currentUser.email
+                }
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(loggedUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('jwt token', data)
+                        localStorage.setItem('user-token', data.token)
+
+                    })
+
+            }
+            else{
+                localStorage.removeItem('user-token')
+            }
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+
+    const userInfo = {
+        user,
+        loading,
+        createUser,
+        logIn,
+        logOut,
+        googleSignIn,
+
+    }
 
 
     return (
--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     const signOut =()=>{
         logOut()
         .then(()=>{
-            localStorage.removeItem('user-token')
+            
         })
         .catch(error=>console.log(error.message))
     }
